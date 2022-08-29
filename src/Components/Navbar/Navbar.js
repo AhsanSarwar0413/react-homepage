82ca8cc78044c0b53b4b133435653200bb8ba8eb
AhsanSarwar0413 <ahsansarwar0413@gmail.com>
2022-08-29 23:59:08 +0500

    main-commit

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const Navigation = styled.header`
+  height: 200px;
+  background: red;  
+`;
+
+function Navbar() {
+  return (
+    <Navigation>
+
+    </Navigation>
+  )
+}
+
+
+export default Navbar
